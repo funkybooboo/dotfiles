9a4d389011813d77375d2b015f670c64e9290105
Nate Stott <nate.stott@pm.me>
2024-12-09 10:30:06 -0700

    add my configs

---
+++ .config/fish/config.fish
@@ -0,0 +1,13 @@
+if status is-interactive
+    alias ls=lsd
+end
+
+function fish_greeting
+
+end
+
+set -gx PATH /home/nate/.config/nixos/ $PATH
+
+function dotfiles
+    /usr/bin/env git --git-dir=$HOME/dotfiles/ --work-tree=$HOME $argv
+end
