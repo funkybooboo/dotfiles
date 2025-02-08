7d6c662dd8f2604a7a6afa722dee40bc60e92773
Nate Stott <nate.stott@pm.me>
2025-02-08 10:36:18 -0500

    88 current  2025-02-08 10:34:47  24.11.714127.f5a32fa27df9  6.6.75                          *

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,4 +13,13 @@ function dotfiles
     /usr/bin/env git --git-dir=$HOME/dotfiles/ --work-tree=$HOME $argv
 end
 
+function yazi
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
+
 fnm env | source
