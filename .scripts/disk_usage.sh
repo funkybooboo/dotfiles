7574301c97951376a69bf4ce389b714c695338cb
Nate Stott <nate.stott@pm.me>
2025-02-18 19:46:33 -0500

    137 current  2025-02-18 19:44:59  24.11.714614.c618e28f7025  6.6.77                          *

--- .scripts/disk_usage.sh
+++ .scripts/disk_usage.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
-df -h / | grep -oP '\d+(?=%)'
+# Set interval (in seconds) and count of reports to display
+INTERVAL=1
+COUNT=10
+
+# Display header
+echo "Disk I/O stats summary:"
+
+# Run iostat to monitor disk I/O
+iostat -dx $INTERVAL $COUNT
