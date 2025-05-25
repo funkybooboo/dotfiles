6dd4d9daff2843589be1846966df16b774e29f8e
Nate Stott <nate.stott@pm.me>
2025-05-25 19:22:03 -0400

    19 current  2025-05-25 19:20:31  24.11.718195.f09dede81861  6.6.92                          *

--- config/.config/nixos/debbie.nix
+++ config/.config/nixos/debbie.nix
@@ -8,11 +8,11 @@
     url = "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
   };
   flatpakApps = [
+    "com.play0ad.zeroad"
     "io.github.voxelcubes.hand-tex"
     "io.github.dman95.SASM"
     "io.github.hamza_algohary.Coulomb"
     "io.gitlab.persiangolf.voicegen"
-    "com.play0ad.zeroad"
   ];
   flatpakAppList = lib.concatStringsSep " " flatpakApps;
 
