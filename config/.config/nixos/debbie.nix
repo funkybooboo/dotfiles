1b38a31000ba403c02de2137957eaa926547a24a
Nate Stott <nate.stott@pm.me>
2025-06-17 11:56:24 -0400

    45 current  2025-06-17 11:54:51  25.05.804113.6c64dabd3aa8  6.12.33                          *

--- config/.config/nixos/debbie.nix
+++ config/.config/nixos/debbie.nix
@@ -466,6 +466,24 @@ in {
                     StandardError = "append:/var/log/syncDocuments.log";
                 };
             };
+            auto-update = {
+                description = "Run NixOS update when idle and on AC power";
+                wantedBy = ["multi-user.target"];
+                after = ["network-online.target"];
+
+                serviceConfig = {
+                    Type = "simple";
+                    Restart = "always";
+                    RestartSec = "3600s";
+                    User = "nate";
+                    # You can hardcode the path or reference it via your link
+                    ExecStart = "${pkgs.bash}/bin/bash /home/nate/.local/bin/auto-update";
+                    # Ensure these paths are set in case your script needs them
+                    Environment = "PATH=/run/current-system/sw/bin:/etc/profiles/per-user/nate/bin:/home/nate/.local/bin";
+                    StandardOutput = "append:/var/log/auto-update.log";
+                    StandardError = "append:/var/log/auto-update.log";
+                };
+            };
         };
     };
 
