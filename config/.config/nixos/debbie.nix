a621494c931ed33f9421b162e637b4a4b3b3abfa
Nate Stott <nate.stott@pm.me>
2025-06-18 21:52:40 -0400

    Update debbie.nix

--- config/.config/nixos/debbie.nix
+++ config/.config/nixos/debbie.nix
@@ -118,7 +118,6 @@ in {
             alsa.enable = true;
             alsa.support32Bit = true;
             pulse.enable = true;
-            wireplumber.enable = true;
         };
         mysql = {
             enable = true;
