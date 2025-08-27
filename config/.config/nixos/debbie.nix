e66f5b1fdfb2f35eee245638425873865608d164
Nate Stott <nate.stott@pm.me>
2025-08-26 22:13:59 -0600

    146 current  2025-08-26 22:12:27  25.05.808723.b1b329146965  6.12.43                          *

--- config/.config/nixos/debbie.nix
+++ config/.config/nixos/debbie.nix
@@ -579,9 +579,9 @@ in {
             wireguard-tools
             openvpn
             # gp-saml-gui
-            openconnect
-            globalprotect-openconnect
-            libsecret
+            # openconnect
+            # globalprotect-openconnect
+            # libsecret
             ipcalc
             nmap
             codecrafters-cli
