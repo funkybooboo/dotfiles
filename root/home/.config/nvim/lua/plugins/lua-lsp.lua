9dfb5374a7530c1036c0622f50b67687c3e203c8
nate <nate.stott@pm.me>
2026-06-08 19:30:30 -0600

    Update neovim LSP plugin configs

--- root/home/.config/nvim/lua/plugins/lua-lsp.lua
+++ root/home/.config/nvim/lua/plugins/lua-lsp.lua
@@ -32,7 +32,7 @@ return {
   },
 
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, {
         "lua-language-server",
